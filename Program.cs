876ef659b2d1b2d410df21133efec2f2faab2b54
Kylian Garnier <kgarnier.inc@gmail.com>
2026-03-07 16:32:56 +0100

    Ajout et création d'une image docker pour le projet front-end

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-using front_end_portefolio;
+using FrontEndPortefolio;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
